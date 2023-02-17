8513744b16f275f86159356f4546e3c2b07c9ab0
Tanmay Chavan <tanmay93chavan@gmail.com>
2023-02-17 20:21:01 +0530

    initial commit

--- Sum.java
+++ Sum.java
@@ -6,6 +6,6 @@ public class Sum {
     public static void main(String[] args) {
         int a=10;int b=20;
         int c=a+b;
-        System.out.println(c);
+        System.out.println("sum "+c);
     }
 }
